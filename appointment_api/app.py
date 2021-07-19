aed90a614f7987f9d4bc3c748b68de4e5dc15d2f
ZhixiangL <lizhixiang27@gmail.com>
2021-07-18 18:59:14 -0700

    add appointment api implementation

---
+++ appointment_api/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+db = SQLAlchemy(app)
+
+
+
+if __name__ == '__main__':
+    from api import *
+    app.run(debug=True)
